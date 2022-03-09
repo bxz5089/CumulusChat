72138ad2b1d4d9e3ea5ccb4655fc4e6834a60126
bxz5089 <69992026+bxz5089@users.noreply.github.com>
2022-03-08 21:33:59 -0500

    Starter files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Chat, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    const chatData = await Chat.findAll({
       include: [
         {
           model: User,
@@ -15,11 +15,11 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const chats = chatData.map((chat) => chat.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', {
-      projects,
+      chats,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -27,9 +27,9 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/project/:id', async (req, res) => {
+router.get('/chat/:id', async (req, res) => {
   try {
-    const projectData = await Project.findByPk(req.params.id, {
+    const chatData = await Chat.findByPk(req.params.id, {
       include: [
         {
           model: User,
---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Chat = require('./Chat');
+
+User.hasMany(Chat, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Chat.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Chat };
