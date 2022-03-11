c2c4ba30f35ec20d97549f80a291df5169d11231
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-03-10 21:06:46 -0400

    css and html added

---
+++ public/js/starter.js
@@ -0,0 +1,5 @@
+document.body.insertAdjacentHTML("afterbegin", "<p>Cumulus Chat</p>");
+const btn = document.createElement("button");
+btn.innerText = "START";
+document.body.appendChild(btn);
+//addevent listener
