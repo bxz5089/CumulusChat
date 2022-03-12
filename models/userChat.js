a5be04cd6ce50c9a7ce5c5e04244feadd0e7c8b9
Genevie Giko <gikogenevie@yahoo.com>
2022-03-12 09:10:30 -0500

    needed to us lint

--- models/userChat.js
+++ models/userChat.js
@@ -1,18 +1,19 @@
-const { doc } = require("prettier")
+const { doc } = require('prettier');
 
 const users = [];
 
 const appendUser = ({ id, name, room }) => {
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
-
   const currentUser = users.find((user) => user.room === room && user.name === name);
 
-  if(!name || !room) 
-  return { error: 'Room and Username are required.' };
+  if(!name || !room){
+    return { error: 'Room and Username are required.' };
+  }
 
-  if(currentUser) 
-  return { error: 'This username is taken.' };
+  if(currentUser) {
+    return { error: 'This username is taken.' };
+  }
 
   const user = { id, name, room };
 
@@ -24,7 +25,8 @@ const appendUser = ({ id, name, room }) => {
 const deleteUser = (id) => {
   const index = users.findIndex((user) => user.id === id);
 
-  if(index !== -1) return users.splice(index, 1)[0];
+  if(index !== -1){
+    return users.splice(index, 1)[0];
 }
 
 const obtainUser = (id) => users.find((user) => user.id === id);
