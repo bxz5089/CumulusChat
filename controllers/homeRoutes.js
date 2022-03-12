f380f8a38b77bd5c5b00c1f1174be7906d3d6674
Genevie Giko <gikogenevie@yahoo.com>
2022-03-12 14:04:22 -0500

    Get the start cloud page to show up

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,28 +3,7 @@ const { Chat, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const chatData = await Chat.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const chats = chatData.map((chat) => chat.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('login', {
-      chats,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  res.render('startcloud');
 });
 
 router.get('/chat/:id', async (req, res) => {
--- models/index.js
+++ models/index.js
@@ -1,8 +1,16 @@
 const User = require('./User');
 const Chat = require('./Chat');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-//create socket on server side
 
 User.hasMany(Chat, {
   foreignKey: 'user_id',
